2fb12e8ae8d756be536f746a0855590b5f7de839
Rodrigo <jcueva@3devnet.com>
2015-05-17 21:47:32 -0500

    BusinessLogic

---
+++ Source/Domain/ColegioVRHT.BusinessLogic/Implementation/BusinessLogicBase.cs
@@ -0,0 +1,52 @@
+﻿using ColegioVRHT.BusinessLogic.Interface;
+using ColegioVRHT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioVRHT.BusinessLogic.Implementation
+{
+    public class BusinessLogicBase<TEntity> : IBusinessLogicBase<TEntity> 
+        where TEntity : EntityBase
+    {
+        private readonly IBusinessLogicBase<TEntity> _BusinessLogicBase;
+
+        public BusinessLogicBase(IBusinessLogicBase<TEntity> _BusinessLogicBase)
+        {
+            this._BusinessLogicBase = _BusinessLogicBase;
+        }
+
+        public void Add(TEntity entity)
+        {
+           _BusinessLogicBase.Add(entity);
+        }
+
+        public TEntity GetById(int Id)
+        {
+            return _BusinessLogicBase.GetById(Id);
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return _BusinessLogicBase.Get();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _BusinessLogicBase.GetAll();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _BusinessLogicBase.Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _BusinessLogicBase.Remove(entity);
+        }
+
+    }
+}
---
+++ Source/Domain/ColegioVRHT.BusinessLogic/Interface/IBusinessLogicBase.cs
@@ -0,0 +1,27 @@
+﻿using ColegioVRHT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioVRHT.BusinessLogic.Interface
+{
+    public interface IBusinessLogicBase<TEntity>
+        where TEntity : EntityBase
+    {
+
+        void Add(TEntity entity);
+
+        TEntity GetById(int Id);
+
+        IQueryable<TEntity> Get();
+
+        IEnumerable<TEntity> GetAll();
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+                
+    }
+}
