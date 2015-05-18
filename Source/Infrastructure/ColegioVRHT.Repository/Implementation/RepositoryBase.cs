f258dbd83c7a71c68217e0e8f8a6e9b3c6cb13e7
Rodrigo <jcueva@3devnet.com>
2015-05-17 21:32:14 -0500

    Repository

--- Source/Infrastructure/ColegioVRHT.Repository/Implementation/RepositoryBase.cs
+++ Source/Infrastructure/ColegioVRHT.Repository/Implementation/RepositoryBase.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using ColegioVRHT.Repository.Interface;
+using ColegioVRHT.Entities;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ColegioVRHT.Repository.Implementation
 {
-    public class RepositoryBase
+    public class RepositoryBase<TEntity, TContext> : IRepositoryBase<TEntity>
+        where TEntity : EntityBase
+        where TContext : DbContext
     {
+        protected TContext Context;
+
+        public RepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public TEntity GetById(int Id)
+        {
+            return Context.Set<TEntity>().Find(Id);
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return Context.Set<TEntity>().AsQueryable();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList().AsEnumerable();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Set<TEntity>().Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+            Context.SaveChanges();
+        }
+
     }
 }
---
+++ Source/Infrastructure/ColegioVRHT.Repository/Interface/IRepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿using ColegioVRHT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColegioVRHT.Repository.Interface
+{
+    public interface IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+
+        void Add(TEntity entity);
+
+        TEntity GetById(int Id);
+
+        IQueryable<TEntity> Get();
+
+        IEnumerable<TEntity> GetAll();
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+    }
+}
